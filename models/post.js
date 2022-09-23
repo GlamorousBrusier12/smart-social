47cccb369398962eae189ea7402c8712752ee317
naveen <pandurebelsachin18@gmail.com>
2022-09-24 01:37:32 +0530

    small change

--- models/post.js
+++ models/post.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose');
-const User = require('./user');
-const postSchema = new mongoose.Schema({
-    content:{
-        type: String,
-        required: true
+const mongoose = require("mongoose");
+const User = require("./user");
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
     },
-    user:{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users", // linking it to the user schemaas
+    },
+    comments: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Users' // linking it to the user schema
-    },
-    comments:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comments' // linking it to the user schema
-        }
-    ]
-},{
-    timestamps: true
-});
+        ref: "Comments", // linking it to the user schema
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // creating a collection which contains the schema instances by mongooose.model('name of th ecollection', schema to be followed)
-const Posts = mongoose.model('Posts', postSchema);
+const Posts = mongoose.model("Posts", postSchema);
 
 module.exports = Posts;
